aa4f0570eaec98d7a11faab8e0460b9f31ad8b67
Timothy Alexander <65827376+taalexander0614@users.noreply.github.com>
2026-04-17 16:22:08 -0400

    Modify OSDCloud start logic based on image availability

--- powershell/osd_config.ps1
+++ powershell/osd_config.ps1
@@ -1,1 +1,10 @@
-Start-OSDCloud -OSEdition Education -Firmware -ZTI
+$imageFileURL = "http://10.24.15.52/LocalSoftwareHosting/image.wim"
+
+$response = Invoke-WebRequest -Uri $imageFileURL -Method Head
+if (!$response) {
+    Start-OSDCloud -OSEdition Education -Firmware -ZTI
+}
+else {
+  Start-OSDCloud -ImageFileURL $imageFileURL -ImageIndex 1 -Firmware
+}
+
