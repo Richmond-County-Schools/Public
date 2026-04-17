c3934b532dd018f447f9a7be3748948b7c9ae255
Timothy Alexander <65827376+taalexander0614@users.noreply.github.com>
2026-04-17 16:35:53 -0400

    Add -Restart option to Start-OSDCloud commands

--- powershell/osd_config.ps1
+++ powershell/osd_config.ps1
@@ -2,9 +2,9 @@ $imageFileURL = "http://10.24.15.52/LocalSoftwareHosting/image.wim"
 
 $response = Invoke-WebRequest -Uri $imageFileURL -Method Head
 if (!$response) {
-    Start-OSDCloud -OSEdition Education -Firmware -ZTI
+    Start-OSDCloud -OSEdition Education -Firmware -ZTI -Restart
 }
 else {
-  Start-OSDCloud -ImageFileURL $imageFileURL -ImageIndex 1 -Firmware
+  Start-OSDCloud -ImageFileURL $imageFileURL -ImageIndex 1 -Firmware -Restart
 }
 
